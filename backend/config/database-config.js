1dd417a2f7728e9ccaf6b1964f8c7f4accc24e6d
shailendra sahu <shailendra2130@gmail.com>
2026-04-07 12:01:56 +0530

    modified:   backend/config/database-config.js 	modified:   backend/package-lock.json 	modified:   backend/package.json

--- backend/config/database-config.js
+++ backend/config/database-config.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import dns from "dns";
 
-dotenv.config();
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+
+dotenv.config({ quiet: true });
 export const connectDB = async () => {
   await mongoose.connect(process.env.MONGODB_URI);
   console.log("MongoDB connected");
