d3841c58fc7458db534e7d74811818ebd29e0709
shailendra sahu <shailendra2130@gmail.com>
2026-04-07 15:14:02 +0530

    added changes in index.js

--- backend/config/cros-config.js
+++ backend/config/cros-config.js
@@ -1,5 +1,8 @@
 app.use(cors({
-    origin: "https://ai-interview-prepration-eta.vercel.app",
+    origin: [
+        "http://localhost:5173",
+        "https://ai-interview-prepration-eta.vercel.app"
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
